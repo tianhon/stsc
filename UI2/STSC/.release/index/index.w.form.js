f4930ef479cfaeb11af33f5af9bb5df8de9de242
tianhon.wt@189.cn <tianhon.wt@189.cn>
2016-08-30 01:37:11 +0800

    commit by wutao to init git repos

---
+++ UI2/STSC/.release/index/index.w.form.js
@@ -0,0 +1,17 @@
+define(function(require){
+require('$model/UI2/system/components/justep/model/model');
+require('$model/UI2/system/components/justep/loadingBar/loadingBar');
+require('$model/UI2/system/components/justep/contents/contents');
+require('$model/UI2/system/components/justep/window/window');
+var __parent1=require('$model/UI2/system/lib/base/modelBase'); 
+var __parent0=require('$model/UI2/STSC/index'); 
+var __result = __parent1._extend(__parent0).extend({
+	constructor:function(contextUrl){
+	this.__sysParam='true';
+	this.__contextUrl=contextUrl;
+	this.__id='';
+	this.__cid='cyEJzQv';
+	this._flag_='460d4e9844e3f35e40c8e85c91f3c8af';
+	this.callParent(contextUrl);
+}}); 
+return __result;});
