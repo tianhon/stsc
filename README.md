57258e0425666b869c027acda3c632eee47fa4bf
tianhon.wt@189.cn <tianhon.wt@189.cn>
2016-09-05 19:07:44 +0800

    add weixin

--- README.md
+++
@@ -1,8 +0,0 @@
-##生态商城
-
-
-- Step 1: Clone this repository.
-- Step 2: Copy all files of the cloned folder into _model_ folder of WeX5 (include _.git_ folder & _.gitignore_ file).
-- Step 3: The _model_ folder of WeX5 now linked with GitHub and you can works with it. 
-
-- **Note:** `.gitignore` file is use to filter files can be track by Git, current setting only *UI2/STSC* will be tracking by Git. refer to below [this link](https://git-scm.com/docs/gitignore) for detail of ignore format pattern.
---
+++ STSC.Service/Controllers/WeiXinController.cs
@@ -0,0 +1,48 @@
+﻿using Senparc.Weixin.MP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace STSC.Service.Controllers
+{
+    public class WeiXinController : Controller
+    {
+
+        public static readonly string Token = WebConfigurationManager.AppSettings["WeixinToken"];
+
+        /// <summary>
+        /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://weixin.senparc.com/weixin
+        /// </summary>
+
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult Get(string signature, string timestamp, string nonce, string echostr)
+        {
+            if (CheckSignature.Check(signature, timestamp, nonce, Token))
+            {
+                return Content(echostr);//返回随机字符串则表示验证通过
+            }
+            else
+            {
+                return Content("failed:" + signature + "," + CheckSignature.GetSignature(timestamp, nonce, Token));
+            }
+        }
+
+        // GET: api/WeiXin/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+        // DELETE: api/WeiXin/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
